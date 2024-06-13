01e20aacf74bc8c9c7be5a7a7e9ef8ec3e7b3954
Florian Schroedl <flo.schroedl@gmail.com>
2024-06-13 11:40:06 +0200

    Add style-dictionary

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -1,4 +1,5 @@
-(ns starter.browser)
+(ns starter.browser
+  (:require ["style-dictionary" :as sd]))
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
@@ -8,7 +9,7 @@
   ;; init is called ONCE when the page loads
   ;; this is called in the index.html and must be exported
   ;; so it is available even in :advanced release builds
-  (js/console.log "init")
+  (js/console.log sd)
   (start))
 
 ;; this is called before any code is reloaded
